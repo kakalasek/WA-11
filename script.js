2bd19e0df0ddcbf6c01366c9c67f731c060e5b5e
kakalasek <honziccekkj@seznam.cz>
2023-12-06 08:20:25 +0100

    hexcock

---
+++ script.js
@@ -0,0 +1,18 @@
+const hexText = document.querySelector("#hexText");
+
+let changeBackground = () => {
+  let currentDatetime = new Date();
+  let currentTime = currentDatetime.toLocaleTimeString(navigator.language, {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  currentTime = currentTime.replace(":", "");
+  currentTime = currentTime.replace(":", "");
+
+  hexText.textContent = "#" + currentTime;
+  document.body.style = `background-color: #${currentTime}`;
+};
+
+setInterval(changeBackground, 1000);
